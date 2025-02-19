0e93933f5da5cd1c27bfd2cc90defcd48e4f917c
RicardoRG96 <ricardoretamal10@gmail.com>
2025-02-19 05:36:36 -0300

    add EntityScan config

--- src/main/java/com/ricardo/scalable/ecommerce/platform/userService/UserServiceApplication.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/userService/UserServiceApplication.java
@@ -2,11 +2,13 @@ package com.ricardo.scalable.ecommerce.platform.userService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EntityScan({"com.ricardo.scalable.ecommerce.platform.libs_common.entities", "com.ricardo.scalable.ecommerce.platform.userService.entities"})
 public class UserServiceApplication {
 
 	@Bean
