a81064a6da51b2cab0d873c15d9cf726891b87d9
RicardoRG96 <ricardoretamal10@gmail.com>
2025-06-25 06:20:25 -0400

    add test to createVerificationToken method in VerificationTokenServiceTest

--- src/test/java/com/ricardo/scalable/ecommerce/platform/userService/services/VerificationTokenServiceTest.java
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/userService/services/VerificationTokenServiceTest.java
@@ -1,5 +1,34 @@
 package com.ricardo.scalable.ecommerce.platform.userService.services;
 
+import static com.ricardo.scalable.ecommerce.platform.userService.services.testData.UserServiceTestData.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.ricardo.scalable.ecommerce.platform.libs_common.entities.User;
+import com.ricardo.scalable.ecommerce.platform.userService.model.repositories.UserRepository;
+
+@SpringBootTest
 public class VerificationTokenServiceTest {
+
+    @MockitoBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private VerificationTokenService verificationTokenService;
+
+    @Test
+    void createVerificationToken_whenUserIsValid_shouldCreateToken() {
+        User user = createUser001().orElseThrow();
+
+        verificationTokenService.createVerificationToken(user);
+
+        assertAll(
+            () -> assertNotNull(user.getVerificationToken())
+        );        
+    }
 
 }
